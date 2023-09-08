42a2a0859a27cf8c03f31cbbc1bca6cbcaf7a9c1
hisashige <hisashige56@gmail.com>
2023-09-08 12:12:01 +0900

    BGM追加

--- src/consts/pomodoro.ts
+++ src/consts/pomodoro.ts
@@ -1,6 +1,10 @@
 import rainAndGuitar from '../assets/sounds/rain_and_guitar.mp3'
 import vanillaSky from '../assets/sounds/Vanilla_Sky_2.mp3'
 import arpeggioOfSeawind from '../assets/sounds/arpeggio_of_seawind.mp3'
+import atSmallCafeInHighland from '../assets/sounds/at_small_cafe_in_highland.mp3'
+import envRain from '../assets/sounds/env_rain.mp3'
+import envWave from '../assets/sounds/env_wave.mp3'
+import envCafe from '../assets/sounds/env_cafe.mp3'
 
 export const POMODORO_TIME = 1500
 
@@ -21,6 +25,30 @@ export const SOUNDS = [
     image: 'https://img.icons8.com/stickers/100/water-element.png',
     label: '海風のアルペジオ',
     value: arpeggioOfSeawind,
-    description: 'エモくなりたい時に。',
+    description: 'エモくなりたい時に',
+  },
+  {
+    image: 'https://img.icons8.com/stickers/100/forest.png',
+    label: '高原の小さなカフェにて',
+    value: atSmallCafeInHighland,
+    description: '柔らかくて暖かいひとときを',
+  },
+  {
+    image: 'https://img.icons8.com/stickers/100/wet.png',
+    label: '環境音:雨',
+    value: envRain,
+    description: '雨の音って落ち着くよね',
+  },
+  {
+    image: 'https://img.icons8.com/stickers/100/ocean-wave.png',
+    label: '環境音:波',
+    value: envWave,
+    description: 'さざ波に揺られて',
+  },
+  {
+    image: 'https://img.icons8.com/stickers/100/restaurant-pickup.png',
+    label: '環境音:カフェ',
+    value: envCafe,
+    description: '喧騒の中で集中したい時に',
   },
 ]
